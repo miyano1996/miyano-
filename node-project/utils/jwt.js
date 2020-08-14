507245ec79d1523212ace9b2a3678756f96e7568
zjf <yll952@haha.com>
2020-08-14 17:56:09 +0800

    hahha

--- node-project/utils/jwt.js
+++ node-project/utils/jwt.js
@@ -7,7 +7,7 @@ const jwtAuth = expressJWT({
     credentialsRequired: true
 }).unless({
     // 配置不需要 token 验证的 url 路径
-    path: ['/users/login','/users/register','/users/isLogin','/managers/login','/managers/register','/managers/isLogin','/users/sign', '/images/upload']
+    path: ['/users/login','/users/register','/users/isLogin','/managers/login','/managers/register','/managers/isLogin','/users/sign', '/images/upload','/managers/loginAdmin']
 });
 
 module.exports = jwtAuth;
