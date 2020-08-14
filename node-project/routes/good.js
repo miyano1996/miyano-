bbda1e5c3c0766a46cc1eb945d6b0282020b7502
Chenshaonan <2445827535@qq.com>
2020-08-14 12:00:53 +0800

    商品修改上下架处理

--- node-project/routes/good.js
+++ node-project/routes/good.js
@@ -34,7 +34,7 @@ router.get('/findAllGoods', async function(req, res, next) {
 });
 // 获取某个商品信息
 router.post('/findGood', async function(req, res, next) {
-    const data = req.query;
+    const data = req.body;
     let msg = await findGood(data);
     if (msg.success) {
         res.send({
@@ -48,6 +48,36 @@ router.post('/findGood', async function(req, res, next) {
         })
     }
 });
-
-
+//修改商品信息
+router.post('/updateGood', async function(req, res, next) {
+    const data = req.body;
+    let msg = await updateGood(data);
+    if (msg.success) {
+        res.send({
+            data: msg.data,
+            success: msg.success
+        })
+    } else {
+        res.send({
+            data: msg.data,
+            success: msg.success
+        })
+    }
+});
+//下架或上架商品
+router.post('/delGood', async function(req, res, next) {
+    const data = req.body;
+    let msg = await delGood(data);
+    if (msg.success) {
+        res.send({
+            data: msg.data,
+            success: msg.success
+        })
+    } else {
+        res.send({
+            data: msg.data,
+            success: msg.success
+        })
+    }
+})
 module.exports = router;
