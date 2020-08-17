a1d48eabdd892776343f00fbbb5166d17e1038f3
YangDehui <yangdehui8842@foxmail.com>
2020-08-17 09:36:14 +0800

    进入店铺重定向到店铺数据 注释了产生bug的请求语句

--- node-project/dao/shopsDao.js
+++ node-project/dao/shopsDao.js
@@ -1,16 +1,20 @@
 const { shopsModel } = require('./../dao/models/shopsModel');
-module.exports.getShops = async data=>{
+module.exports.getShops = async data => {
     // console.log(data);
-    return await shopsModel.find({managerId:data.managerId})
+    return await shopsModel.find({ managerId: data.managerId })
 }
-module.exports.addShops = async data=>{
+module.exports.addShops = async data => {
     // console.log(data);
     return await shopsModel.create(data)
 }
-module.exports.delShops = async data=>{
+module.exports.delShops = async data => {
     return await shopsModel.deleteOne(data)
 }
-module.exports.updateShops =  async data=>{
+module.exports.updateShops = async data => {
     // console.log(data);
-    return await shopsModel.updateOne({ _id:data._id }, { ...data });
+    return await shopsModel.updateOne({ _id: data._id }, { ...data });
+}
+module.exports.getOneShop = async _id => {
+    console.log(_id)
+    return await shopsModel.find({ _id })
 }
--- vue-project/src/components/shops/shopCharts.vue
+++ vue-project/src/components/shops/shopCharts.vue
@@ -1,18 +1,76 @@
 <template>
-    <div>
-        <shop-data></shop-data>
-    </div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>数据总览</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <shop-data></shop-data>
+    </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>销售专项</span>
+      </div>
+      <shop-sales></shop-sales>
+    </el-card>
+    <!-- <type-sales></type-sales> -->
+  </div>
 </template>
 
 <script>
-import ShopData from '../charts/ShopData/ShopData'
+import ShopData from "../charts/ShopData/ShopData";
+import ShopSales from "../charts/ShopSales/ShopSales";
+import TypeSales from "../charts/ShopSales/TypeSales";
+import { createNamespacedHelpers } from "vuex";
+const { mapState, mapActions, mapMutations } = createNamespacedHelpers("shops");
 export default {
-    components:{
-        ShopData
-    }
-}
+  components: {
+    ShopData,
+    ShopSales,
+    TypeSales,
+  },
+  data(){
+      return {
+          shopData:{}
+      }
+  },
+  computed: {
+    ...mapState(['shopsId',"getOneShop"]),
+
+  },
+ async created(){
+      const data = await this.getOneShopSync()
+      if(data.success) {
+        this.shopData = this.getOneShop;
+        console.log(this.shopData)
+      }
+  },
+  methods: {
+      ...mapActions(['getOneShopSync'])
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.text {
+  font-size: 14px;
+}
 
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 100%;
+  margin-bottom: 50px;
+}
 </style>
