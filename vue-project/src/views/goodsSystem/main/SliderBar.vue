94538e23042f6411f87ece2a10ec1f570ff724d0
XueYanBo <739233662@qq.com>
2020-08-17 10:55:30 +0800

    添加了详情图片多张上传功能

--- vue-project/src/views/goodsSystem/main/SliderBar.vue
+++ vue-project/src/views/goodsSystem/main/SliderBar.vue
@@ -29,15 +29,14 @@
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="2" v-if="shopsId">
-          <template slot="title" >
+          <template slot="title">
             <i class="el-icon-goods"></i>
             <span>商品中心</span>
           </template>
-          <el-menu-item-group >
+          <el-menu-item-group>
             <el-menu-item index="/main/shopCharts">店铺数据</el-menu-item>
-            <el-menu-item index="/main/goodsList" >商品列表</el-menu-item>
+            <el-menu-item index="/main/goodsList">商品列表</el-menu-item>
             <el-menu-item index="/main/addGood">添加商品</el-menu-item>
-            <el-menu-item index="2-2">商品分类</el-menu-item>
             <el-menu-item index="/main/notListedGoods">未上架商品</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
@@ -49,43 +48,24 @@
           <el-menu-item-group>
             <el-menu-item index="/main/goodOrderList">订单列表</el-menu-item>
             <el-menu-item index="/main/recycledOrders">订单还原</el-menu-item>
-            <el-menu-item index="3-3">退货申请处理</el-menu-item>
-            <el-menu-item index="3-4">退货原因设置</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
-        <el-submenu index="4" v-if="shopsId">
+        <el-submenu index="5" :style="{'display': str}">
           <template slot="title">
-            <i class="el-icon-s-marketing"></i>
-            <span>营销</span>
+            <i class="el-icon-key"></i>
+            <span>管理员中心</span>
           </template>
           <el-menu-item-group>
-            <el-menu-item index="4-1">活动管理</el-menu-item>
-            <el-menu-item index="4-2">专场活动</el-menu-item>
-            <el-menu-item index="4-3">促销专区</el-menu-item>
+            <el-menu-item index="/shopManager">店铺管理</el-menu-item>
+            <el-menu-item index="/messageCenter">消息中心</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
-        
-          <el-submenu index="5" :style="{'display': str}">
-            <template slot="title">
-              <i class="el-icon-key"></i>
-              <span>管理员中心</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="5-1">系统管理</el-menu-item>
-              <el-menu-item index="/shopManager">店铺管理</el-menu-item>
-              <el-menu-item index="5-3">用户管理</el-menu-item>
-              <el-menu-item index="5-4">产品管理</el-menu-item>
-              <el-menu-item index="/messageCenter">消息中心</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        
       </el-menu>
     </el-col>
   </el-row>
 </template>
 
 <script>
-
 import { createNamespacedHelpers } from "vuex";
 const { mapState } = createNamespacedHelpers("shops");
 
@@ -93,11 +73,11 @@ export default {
   data() {
     return {
       isAdmin: false,
-      str:'none'
+      str: "none",
     };
   },
-  computed:{
-    ...mapState(['shopsId'])
+  computed: {
+    ...mapState(["shopsId"]),
   },
   created() {
     this.checkAdmin();
@@ -105,10 +85,10 @@ export default {
   methods: {
     checkAdmin() {
       this.isAdmin = JSON.parse(localStorage.getItem("isAdmin"));
-      if(this.isAdmin){
-        this.str = 'block'
-      }else{
-        this.str = 'none'
+      if (this.isAdmin) {
+        this.str = "block";
+      } else {
+        this.str = "none";
       }
     },
   },
@@ -117,7 +97,6 @@ export default {
 
 <style scoped>
 .el-col {
-  width: 240px;
-  
+  width: 100%;
 }
 </style>
